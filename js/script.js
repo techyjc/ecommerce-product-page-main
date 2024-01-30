1cb91e67c70024e6ed8e30ba78f7c2a88a015af2
techyjc <85077790+techyjc@users.noreply.github.com>
2024-01-30 00:42:03 +0000

    updated carousel and minor changes

--- js/script.js
+++ js/script.js
@@ -3,8 +3,23 @@ const ecommerce = document.querySelector('.ecommerce-wrapper');
 const img_carousel = document.querySelectorAll('.product-carousel-image');
 const img_carousel_thumb = document.querySelectorAll('.thumb');
 
+const product_carousel = document.querySelector('.carousel-images');
+const float_carousel = document.querySelector('.floating-carousel');
+
 const forward = document.querySelector('.btn-right');
 const backward = document.querySelector('.btn-left');
+
+// --------------
+
+const img_carousel2 = document.querySelectorAll('.product-carousel-image-2');
+const img_carousel_thumb2 = document.querySelectorAll('.thumb-2');
+
+const forward2 = document.querySelector('.btn-right-2');
+const backward2 = document.querySelector('.btn-left-2');
+
+// --------------
+
+const close_carousel = document.querySelector('.close-carousel');
 
 const decrease_qty = document.querySelector('.dec-qty');
 const increase_qty = document.querySelector('.inc-qty');
@@ -18,9 +33,26 @@ const my_cart = document.querySelector('.mycart-contents');
 const add2basket = document.querySelector('.add-2-cart');
 
 let qty = total_qty.innerHTML;
+let floatactive = 'false';
 
 let current_index = -1;
 let new_index = -1;
+
+product_carousel.addEventListener('click', (e) => {
+    if (e.target.classList.contains("product-carousel-image")) {
+        let floatactive = 'false';
+        floatactive = float_carousel.getAttribute('data-visible');
+        if (floatactive == 'false') {
+            float_carousel.setAttribute('data-visible', 'true');
+        } else {
+            float_carousel.setAttribute('data-visible', 'false');
+        }
+    }
+});
+
+close_carousel.addEventListener('click', (e) => {
+        float_carousel.setAttribute('data-visible', 'false');
+});
 
 
 function find_current() {
@@ -57,22 +89,75 @@ function thumbclick() {
 function setimage(sel_index) {
     img_carousel.forEach(function (sel_image, img_index, arr) {
         let img_select = sel_image;
-        if(img_index != sel_index){
-            sel_image.setAttribute('data-current','false');
-        }else{
-            sel_image.setAttribute('data-current','true');
+        if (img_index != sel_index) {
+            sel_image.setAttribute('data-current', 'false');
+        } else {
+            sel_image.setAttribute('data-current', 'true');
         }
     });
     img_carousel_thumb.forEach(function (sel_image, tmb_index, arr) {
         let tmbimg_select = sel_image;
 
-        if(tmb_index != sel_index){
-            tmbimg_select.setAttribute('data-current','false');
-        }else{
-            tmbimg_select.setAttribute('data-current','true');
+        if (tmb_index != sel_index) {
+            tmbimg_select.setAttribute('data-current', 'false');
+        } else {
+            tmbimg_select.setAttribute('data-current', 'true');
         }
     });
 }
+
+function find_current2() {
+    img_carousel2.forEach(function (c_image, index, arr) {
+        let img_select = c_image.getAttribute('data-current');
+
+        if (img_select === "true") {
+            current_index = index;
+        }
+    });
+    return current_index;
+}
+
+function find_currentThumb2() {
+    img_carousel_thumb2.forEach(function (c_image, index, arr) {
+        let img_select = c_image.getAttribute('data-current');
+
+        if (img_select === "true") {
+            current_index = index;
+        }
+    });
+    return current_index;
+}
+
+function thumbclick2() {
+    img_carousel_thumb2.forEach(function (c_image, index, arr) {
+        let img_select = c_image.addEventListener('click', (e) => {
+            setimage2(index)
+        });
+    });
+}
+
+function setimage2(sel_index) {
+    img_carousel2.forEach(function (sel_image, img_index, arr) {
+        let img_select = sel_image;
+
+        if (img_index != sel_index) {
+            sel_image.setAttribute('data-current', 'false');
+        } else {
+            sel_image.setAttribute('data-current', 'true');
+        }
+    });
+    img_carousel_thumb2.forEach(function (sel_image, tmb_index, arr) {
+        let tmbimg_select = sel_image;
+
+        if (tmb_index != sel_index) {
+            tmbimg_select.setAttribute('data-current', 'false');
+        } else {
+            tmbimg_select.setAttribute('data-current', 'true');
+        }
+    });
+}
+
+
 
 function change_qty(qtyaction, event = "") {
 
@@ -147,6 +232,7 @@ function removefromcart(cartitem) {
     cartitem.remove();
 }
 
+
 forward.addEventListener('click', (e) => {
     current_index = find_current();
 
@@ -182,6 +268,46 @@ backward.addEventListener('click', (e) => {
         img_carousel_thumb[current_index].setAttribute('data-current', 'false');
     }
 });
+
+
+forward2.addEventListener('click', (e) => {
+    console.log('pressed f2');
+    current_index = find_current2();
+
+    if (current_index <= img_carousel2.length - 1) {
+
+        if (new_index < img_carousel2.length - 1) {
+            new_index = current_index + 1;
+        } else {
+            new_index = 0;
+        }
+        img_carousel2[new_index].setAttribute('data-current', 'true');
+        img_carousel2[current_index].setAttribute('data-current', 'false');
+        img_carousel_thumb2[new_index].setAttribute('data-current', 'true');
+        img_carousel_thumb2[current_index].setAttribute('data-current', 'false');
+    }
+});
+
+backward2.addEventListener('click', (e) => {
+    console.log('pressed f2');
+    current_index = find_current2();
+
+    if (current_index > 0) {
+
+        new_index = current_index - 1;
+        img_carousel2[new_index].setAttribute('data-current', 'true');
+        img_carousel2[current_index].setAttribute('data-current', 'false');
+        img_carousel_thumb2[new_index].setAttribute('data-current', 'true');
+        img_carousel_thumb2[current_index].setAttribute('data-current', 'false');
+    } else {
+        new_index = 3;
+        img_carousel2[new_index].setAttribute('data-current', 'true');
+        img_carousel2[current_index].setAttribute('data-current', 'false');
+        img_carousel_thumb2[new_index].setAttribute('data-current', 'true');
+        img_carousel_thumb2[current_index].setAttribute('data-current', 'false');
+    }
+});
+
 
 decrease_qty.addEventListener("click", (e) => {
     change_qty("minus");
@@ -237,3 +363,4 @@ cart_wrapper.addEventListener('mouseleave', (e) => {
 })
 
 thumbclick();
+thumbclick2();
